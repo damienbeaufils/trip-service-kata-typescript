05b59735174851db84dbd02293642a8241eb14e7
Damien Beaufils <damien.beaufils@gmail.com>
2021-04-22 09:35:12 -0400

    Initial commit

---
+++ src/trip/TripService.ts
@@ -0,0 +1,37 @@
+import UserNotLoggedInException from "../exception/UserNotLoggedInException";
+import User from "../user/User";
+import UserSession from "../user/UserSession";
+import Trip from "./Trip";
+import TripDAO from "./TripDAO";
+
+export default class TripService {
+
+    /**
+     * find trips of given user
+     * @param u the user
+     * @return trips found
+     * @throws UserNotLoggedInException
+     */
+    public getTripsByUser(u: User): Trip[] {
+        let trips = [];
+        const logged: any = UserSession.getLoggedUser();
+        let f: boolean = false;
+
+        if (logged != null) {
+            for (const u1 of u.getFriends()) {
+                if (u1 === logged) {
+                    f = true;
+                    break;
+                }
+            }
+
+            if (f) {
+                trips = TripDAO.findTripsByUser(u);
+            }
+
+            return trips;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
+}
