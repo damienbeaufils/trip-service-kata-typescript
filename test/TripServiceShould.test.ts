92be835a750e41c9bf98652d478df74c26eef006
Philippe Hebert <philippe@human.space>
2021-04-29 09:37:05 -0400

    wip

--- test/TripServiceShould.test.ts
+++ test/TripServiceShould.test.ts
@@ -1,13 +1,18 @@
 import "jest";
 import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
+jest.mock("../src/user/UserSession")
+import UserSession from "../src/user/UserSession";
 import TripService from "../src/trip/TripService";
+
+const UserSession: UserSession = jest.createMockFromModule("../src/user/UserSession");
 
 describe("TripServiceShould", () => {
 
-    it('huwfehfweuighiowehgweoi', () => {
+    it('should throw an UserNotLoggedInException if the user is not logged in', () => {
         // given
         const tripService = new TripService();
         const user = null;
+        UserSession.getLoggedUser = jest.fn(() => null)
 
         // when
         const result = () => tripService.getTripsByUser(user);
