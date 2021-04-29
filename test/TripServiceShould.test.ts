0d993280e1fecae954c467c7dce924b9c27cdad9
Damien Beaufils <damien.beaufils@gmail.com>
2021-04-29 09:28:04 -0400

    🚧 WIP

--- test/TripServiceShould.test.ts
+++ test/TripServiceShould.test.ts
@@ -1,7 +1,18 @@
 import "jest";
+import UserNotLoggedInException from "../src/exception/UserNotLoggedInException";
+import TripService from "../src/trip/TripService";
 
 describe("TripServiceShould", () => {
-    it("...", () => {
-        expect(4 + 4).toBe(9);
+
+    it('huwfehfweuighiowehgweoi', () => {
+        // given
+        const tripService = new TripService();
+        const user = null;
+
+        // when
+        const result = () => tripService.getTripsByUser(user);
+
+        // then
+        expect(result).toThrow(new UserNotLoggedInException());
     });
 });
